64db5c7ccd0c3c59aa64b81b24c28e4b247971ae
silemi5 <emirjojr5@gmail.com>
2020-10-23 13:25:12 +0800

    feat: implements create user feat: adds userSchema and User model refactor: make app a module

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,15 @@
 import Koa from "koa";
 import Router from "koa-router";
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import { User } from './model'
+import bodyParser from 'koa-bodyparser'
 
-const app = new Koa();
+dotenv.config();
+
+export const app = new Koa();
 const router = new Router();
-
-
-export interface User {
-  id?: string;
-  name: string;
-  email: string;
-}
-
-export const server = app.listen(3000);
+const uri: string = process.env.CONNECTION_URI || ""
 
 // route definitions
 router
@@ -21,15 +19,34 @@ router
   .post('/api/users', createUser)
   .post('/api/auth', authenticateUser)
 
+app.use(bodyParser())
 app.use(router.routes())
 
-module.exports = {
-  server
-};
+// mongoose
+mongoose.connect(uri)
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'Connection error: '));
+db.once('open', () => {
+  app.listen(3000)
+});
+
 
 
 async function createUser(ctx) {
   // TODO: Create a user
+  const request = ctx.request.body
+  await User.create({ email: request.email, password: request.password, name: request.name || null })
+    .then((res) => {
+      ctx.body = {
+        id: res._id,
+        email: res.email,
+        name: res.name
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 
 }
 
